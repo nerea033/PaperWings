c476120fde7dc0e648e6f33bad647c888282e2e4
nerea033 <happynerea07@gmail.com>
2024-04-24 18:34:41 +0200

    Initial commit

---
+++ app/src/main/java/es/dam/paperwings/model/Inventory.kt
@@ -0,0 +1,4 @@
+package es.dam.paperwings.model
+
+class Inventory {
+}
---
+++ app/src/main/java/es/dam/paperwings/view/Login.kt
@@ -0,0 +1,58 @@
+package es.dam.paperwings.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import es.dam.paperwings.R
+
+class Login : AppCompatActivity() {
+
+    // Declaro los elementos de mi vista
+    private lateinit var tbUsername: EditText
+    private lateinit var tbPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var linkRegister: TextView
+
+    // Inicializo la actividad
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Inicializo los elementos de la vista
+        tbUsername = findViewById(R.id.tbUsernameLogin)
+        tbPassword = findViewById(R.id.tbPswLogin)
+        btnLogin = findViewById(R.id.btnLogin)
+        linkRegister = findViewById(R.id.linkRegister)
+
+        // Establecer un evento clic para cambiar a la actividad de registro
+        linkRegister.setOnClickListener {
+            irARegistro(R.layout.activity_register)
+            finish()
+        }
+
+        // Establecer un evento clic para ir a la pantalla de Libros
+        btnLogin.setOnClickListener {iniciarSesion()}
+
+    }
+
+    /**
+     * Cambia a la actividad de registro.
+     * @param [layoutId] ID del diseño de la vista de registro.
+     */
+    fun irARegistro(layoutId: Int) {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+}
