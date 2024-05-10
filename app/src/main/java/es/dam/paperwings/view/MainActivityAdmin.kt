ec81f9b2fde65b6986079841c684f870b92b1692
nerea033 <happynerea07@gmail.com>
2024-05-10 19:40:40 +0200

    Bottom navigation user & admin design v1.3, con fragments

--- app/src/main/java/es/dam/paperwings/view/MainActivityAdmin.kt
+++ app/src/main/java/es/dam/paperwings/view/MainActivityAdmin.kt
@@ -5,9 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import es.dam.paperwings.R
+import es.dam.paperwings.view.fragments.AddFragment
+import es.dam.paperwings.view.fragments.DeleteFragment
+import es.dam.paperwings.view.fragments.HomeFragment
+import es.dam.paperwings.view.fragments.MenuFragment
+import es.dam.paperwings.view.fragments.ProfileFragment
+import es.dam.paperwings.view.fragments.SearchFragment
+import es.dam.paperwings.view.fragments.UpdateFragment
 
 class MainActivityAdmin : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +29,38 @@ class MainActivityAdmin : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        bottomNavigationView = findViewById(R.id.botton_navigation_admin)
+
+        // Transiciones entre fragments
+        bottomNavigationView.setOnItemSelectedListener {menuItem ->
+            when (menuItem.itemId){
+                R.id.bottom_add -> {
+                    replaceFragment(AddFragment())
+                    true
+                }
+                R.id.bottom_update -> {
+                    replaceFragment(UpdateFragment())
+                    true
+                }
+                R.id.bottom_delete -> {
+                    replaceFragment(DeleteFragment())
+                    true
+                }
+                R.id.bottom_profile -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+
+        }
+
+        // Fragmento inicial
+        replaceFragment(AddFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container_admin, fragment).commit()
     }
 }
